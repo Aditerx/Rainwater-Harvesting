6307231373bef6f4546556b76ce4843b91e96ae2
Aditerx <phattai3178@gmail.com>
2021-12-25 09:21:21 +0700

    Upload Dec 25, 2021

--- script/js.js
+++ script/js.js
@@ -1,35 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
-app.config(function($routeProvider) {
-
-    $routeProvider
-
-        .when("/",{
-            templateUrl: "index.html"
-
-        })
-        .when("/res",{
-            templateUrl: "residential.html"
-
-        })
-        .when("/com",{
-            templateUrl: "commercial.html"
-
-        })
-        .when("/mets",{
-            templateUrl: "method.html"
-
-        })
-        .when("/devs",{
-            templateUrl: "development.html"
-
-        })
-        .when("/cont",{
-            templateUrl: "contact.html"
-
-        });
-});
-
 // app.controller("mainCtrl", function($scope, $http) {
 
 //     $http.get("json.json").then(function(response) {
