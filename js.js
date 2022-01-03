07f160b0a7ae4c7e82187ba8b4c2b52e3e50c4a9
Aditerx <phattai3178@gmail.com>
2022-01-03 23:07:19 +0700

    Update Jan 03, 2022

--- js.js
+++ js.js
@@ -6,7 +6,6 @@ app.run(function ($rootScope, $http) {
     $http.get("accessories.json").then(function (response) {
         $rootScope.accessories = response.data.accessories;
         console.log($rootScope.accessories);
-
     });
-    
+
 });
