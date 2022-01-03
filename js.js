494a5a03e0dd844bc1a80807ec7844e2f6c8528d
Aditerx <phattai3178@gmail.com>
2022-01-04 03:23:05 +0700

    Update Jan 03, 2022

--- js.js
+++ js.js
@@ -8,4 +8,19 @@ app.run(function ($rootScope, $http) {
         console.log($rootScope.accessories);
     });
 
+    $http.get("pumpControl.json").then(function (response) {
+        $rootScope.pumpControl = response.data.pumpControl;
+        console.log($rootScope.pumpControl);
+    });
+
+    $http.get("water_sterilisation.json").then(function (response) {
+        $rootScope.water_sterilisation = response.data.water_sterilisation;
+        console.log($rootScope.water_sterilisation);
+    });
+
+    $http.get("tanks.json").then(function (response) {
+        $rootScope.tanks = response.data.tanks;
+        console.log($rootScope.tanks);
+    });
+
 });
